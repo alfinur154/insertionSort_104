566eb3be6b4625f8b1688f013c7da9fb770fd788
alfinur154 <alfinur27@gmail.com>
2023-03-23 23:08:18 +0700

    prosedur input

--- insertionSort_104/insertionSort_104.cpp
+++ insertionSort_104/insertionSort_104.cpp
@@ -3,3 +3,27 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << " Masukkan jumlah data pada array : ";
+		cin >> n;
+
+
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
