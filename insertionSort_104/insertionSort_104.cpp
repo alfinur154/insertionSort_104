7129f64917dade7037997f9ca584bc31be53d562
alfinur154 <alfinur27@gmail.com>
2023-03-23 23:18:33 +0700

    menampilkan kondisi

--- insertionSort_104/insertionSort_104.cpp
+++ insertionSort_104/insertionSort_104.cpp
@@ -21,21 +21,13 @@ void input() {
 	cout << "Masukkan Element Aray" << endl;
 	cout << "===================" << endl;
 
-
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
 	}
 
+}
+
 
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
